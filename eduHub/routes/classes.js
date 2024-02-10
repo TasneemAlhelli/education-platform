9598fea586b3b0edee336579a32c7e0fc772ed5a
ZahraaAlhawaj <zahraa.mu2019@gmail.com>
2024-02-10 10:18:39 +0300

    create edit and delete buttons

--- eduHub/routes/classes.js
+++ eduHub/routes/classes.js
@@ -3,15 +3,21 @@ const router = express.Router()
 
 const classesCtrl = require('../controllers/classes')
 
-//get /
+//get /classes
 router.get('/', classesCtrl.index)
+//only specific classes
+//get /classes
+router.get('/myclasses', classesCtrl.index)
+
 //get /classes/new
 router.get('/new', classesCtrl.new)
 //post /classes
-router.post('/new', classesCtrl.create)
+router.post('/', classesCtrl.create)
 // get /classes/:id
 router.get('/:id', classesCtrl.show)
 //post /classes/:id
 router.post('/:id', classesCtrl.enroll)
+//delete /classes/:id
+router.delete('/myclasses/:id', classesCtrl.delete)
 
 module.exports = router
