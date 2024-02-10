2c600576e792863beb7247b9ec8ec7df349fdeb9
Ali Albanna <ali.al-banna@outlook.com>
2024-02-10 21:33:58 +0300

    posts form created and routed and controlled

--- eduHub/controllers/posts.js
+++ eduHub/controllers/posts.js
@@ -1,11 +1,21 @@
 const Class = require("../models/class");
 
+// show the form to create a new post
 const newPostForm = (req, res) => {
-  res.send("form for adding new post");
+  classId = req.params.classId;
+  res.render("posts/new", {title: "New Post", classId});
 };
 
+//save to DB the new post
 const addPostToClass = async (req, res) => {
-  res.send("posts added to class");
+  message = req.body.message;
+  heading = req.body.heading;
+  classId = req.params.classId;
+  
+  const subject = await Class.findById(classId);
+  console.log("🚀 ~ addPostToClass ~ subject:", subject)
+  res.send("post posted");
+
 }
 
 module.exports = {
